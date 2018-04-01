cac704faf6528cd9c4055a72b4c8be7f3833bd8e
super2b <super2b@qq.com>
2018-04-02 00:31:32 +0800

    add eureka-server support

--- services/src/main/java/com/super2b/Super2bApplication.java
+++ services/src/main/java/com/super2b/Super2bApplication.java
@@ -2,15 +2,11 @@ package com.super2b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-//@EnableEurekaServer
 public class Super2bApplication {
 
 	public static void main(String[] args) {
