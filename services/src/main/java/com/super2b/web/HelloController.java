a7df0f839d28ac83280aba5ee200b0603b65aacb
super2b <super2b@qq.com>
2018-04-03 21:21:15 +0800

    add an Eureka client that regist to the server

--- services/src/main/java/com/super2b/web/HelloController.java
+++ services/src/main/java/com/super2b/web/HelloController.java
@@ -3,9 +3,9 @@ package com.super2b.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
-  @GetMapping("/hello") public User index() {
-    return new User("lwb", "123456", "1");
-  }
-}
+//@RestController
+//public class HelloController {
+//  @GetMapping("/hello") public User index() {
+//    return new User("lwb", "123456", "1");
+//  }
+//}
